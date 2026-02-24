c4304ef15b4009bca314910d871613acf5bfe8d4
vansh mundhra <vanshmundhra@vanshs-MacBook-Air.local>
2026-02-24 13:05:03 +0530

    rep

--- backend/test_gemini.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAWxcPAZg2vnEc5sP_2Ezz1yy5H7JsaN6I")
-model = genai.GenerativeModel("models/gemini-2.5-flash")
-
-
-response = model.generate_content("Say hello in one sentence.")
-print(response.text)
